60b9bf07d4ec880d37e6283a12e8ee04ce32294a
Braedon <braedondavies@gmail.com>
2019-05-31 10:05:33 -0600

    Updating files.

--- graphs/pie-graph.js
+++ graphs/pie-graph.js
@@ -1,27 +1,21 @@
-/*
-https://youtu.be/8ZY4-SpNXc8
-*/
-
-
-google.charts.load('current', { 'packages' :['corechart']});
+google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-    var data = google.visualization.arrayToDataTable({
-        ['Task', 'Hours Per Day'],
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
         ['Work', 11],
+        ['Nap', 2],
         ['Eat', 2],
-        ['Commute', 2],
         ['Watch TV', 2],
-        ['Sleep', 7],
-    });
+        ['Sleep', 7]
+    ]);
 
     var options = {
         title: 'My Daily Activities'
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('Piehart'));
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-    chart.draw(data.options);
-
+    chart.draw(data, options);
 }
---
+++ scss-var/scss-var.js
@@ -0,0 +1,14 @@
+
+const colorInput = document.querySelector('input[type=color]')
+const colorButton = document.querySelector('button')
+const colorVariable = '--yellow'
+
+// _ is equivalent to function()
+colorButton.addEventListener('click', _ => {
+  document.documentElement.style.setProperty(colorVariable, 'green')
+})
+
+colorInput.addEventListener('change', e => {
+  //console.log(e.target.value)
+  document.documentElement.style.setProperty(colorVariable, e.target.value)
+})
